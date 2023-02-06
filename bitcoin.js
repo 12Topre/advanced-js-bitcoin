1ffe299a04fec506a475a37cc218f9504db6c9cf
12Topre <114716091+12Topre@users.noreply.github.com>
2023-02-06 16:56:49 -0500

    Add files via upload

---
+++ bitcoin.js
@@ -0,0 +1,14 @@
+class BitcoinPurchaseFrequency {
+    constructor(id, purchaseFrequency) {
+      this.id = id;
+    
+      this.purchaseFrequency = purchaseFrequency;
+    }
+
+    minimumFractionDigits: 2
+});
+return formatter.format(this.purchaseFrequency);
+}
+} 
+
+/* Not sure about this one, I guessed */ 
